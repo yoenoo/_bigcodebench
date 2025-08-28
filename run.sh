b1a732b0969098485552760a7f124d5c7517ea14
Yeonwoo Jang <yjang385@gmail.com>
2025-08-28 21:06:49 +0000

    add more dependencies

--- run.sh
+++ run.sh
@@ -6,9 +6,12 @@ uv pip install flash-attn --no-build-isolation
 
 uv pip install matplotlib scikit-learn seaborn folium scikit-image nltk statsmodels bs4 faker flask openpyxl xmltodict pyquery cryptography
 uv pip install django holidays geopy tensorflow keras sendgrid docx xlwt prettytable flask_mail natsort chardet pytesseract docx wordninja textblob python-Levenshtein
-# uv pip install mechanize texttable exceptions pycryptodome gensim pyfakefs
+uv pip install mechanize texttable exceptions pycryptodome gensim pyfakefs
 uv pip install wordcloud wikipedia flask_login flask_restful librosa requests_mock mechanize texttable python-docx pycryptodome gensim pyfakefs
-apt-get install python3-tk
+uv pip install flask_wtf geopandas 
+
+apt-get update && apt-get install -y python3-tk
+apt-get update && apt-get install -y python3.10-dev build-essential
 
  
 DATASET=bigcodebench
@@ -26,6 +29,7 @@ bigcodebench.evaluate \
   --execution "local" \
   --backend $BACKEND \
   --temperature 1.0 \
+  --max_new_tokens 20000 \
   --pass_k "1,5,10"
 
 # bigcodebench.inspect \
